d8670aae0119ac59ebf01c4d310c4bf83a7c586d
Kristian Martinez <kmartman94@gmail.com>
2020-10-16 20:49:01 -0700

    updated shopData, updated car utils with add items to cart, updated cart such that quantity of each item is tracked

--- src/components/collection-item/CollectionItem.component.js
+++ src/components/collection-item/CollectionItem.component.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import {connect} from 'react-redux';
+import {addItem} from '../../redux/cart/cart.actions';
+
+import CustomButton from '../custom-button/CustomButton.component';
+
 import './collection-item.styles.scss'
 
-const CollectionItem = ({id, name, imageUrl, price}) => {
+const CollectionItem = ({item, addItem}) => {
+
+    const {id, name, price, imageUrl} = item
     return (
         <div className='collection-item'>
             <div 
@@ -13,8 +20,15 @@ const CollectionItem = ({id, name, imageUrl, price}) => {
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
+            <CustomButton inverted onClick={() => addItem(item)}>Add to cart</CustomButton>
         </div>
     );
 }
 
-export default CollectionItem;
+function mapDispatchToProps(dispatch) {
+    return {
+        addItem: item => dispatch(addItem(item))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
